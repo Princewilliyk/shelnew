cf4c177d0f16381f00eae3035b01c6b2af3c18b3
princewilliyk <Uka Princewill>
2023-05-23 00:31:06 -0700

    task update

--- shell.h
+++ shell.h
@@ -1,8 +1,29 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <stdlib.h>
 #include <unistd.h>
+#include <stdarg.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <dirent.h>
+#include <errno.h>
+#include <signal.h>
 
+/* handle strings print*/
 int _putchar(char c);
+int _puts(const char *str);
+int _put(const char *str);
+void print_digits(unsigned int n);
+void print(char **av);
+
+/*getline function proto*/
+ssize_t _getlines(char **lineptr, size_t *n, FILE *stream);
+
+void shprompt(char **av, char **env);
 
 #endif
